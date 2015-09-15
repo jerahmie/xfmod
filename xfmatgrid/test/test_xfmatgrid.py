613b147fea73d8de8d52376ef52f713e9f6e0c1e
Jerahmie Radder <jerahmie@uwalumni.com>
2015-09-15 17:00:29 -0500

    Renamed tests/ to test/ to be compliant with python unittest module.

---
+++ xfmatgrid/test/test_xfmatgrid.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+"""
+Test xfmatgrid module.
+"""
+
+from __future__ import(division, print_function, unicode_literals)
+
+import sys, os
+import unittest
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(),
+                                            os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+from xfmatgrid import *
+
+TEST_PROJECT_DIR = os.path.realpath(os.path.relpath(os.path.join('..', '..',
+                                                            'Test_Data',
+                                                            'Test_Coil.xf')))
+MULTIPOINT_SENSOR_FILE = os.path.realpath(os.path.join(TEST_PROJECT_DIR,
+                                'Simulations', '000001', 'Run0001', 'output',
+                                'MultiPoint_Solid_Sensor1_0_info.bin'))
+
+class TestXFMatGrid(unittest.TestCase):
+    """Tests for xfmatgrid module."""
+    def setUp(self):
+        self.grid = XFFieldNonUniformGrid()
+        self.grid.sim_id = 1
+
+    def test_xf_run_id_to_str(self):
+        """Verify XFdtd valid run string composition."""
+        self.assertTrue(is_valid_run_id(1))
+        self.assertTrue(is_valid_run_id(9999))
+        self.assertFalse(is_valid_run_id(-1))
+        self.assertFalse(is_valid_run_id(0))
+        self.assertFalse(is_valid_run_id(10000.0))
+        self.assertEqual('Run0001',xf_run_id_to_str(1))
+        self.assertEqual('Run9999',xf_run_id_to_str(9999))
+
+    def test_xf_sim_id_to_str(self):
+        """Verify XFdtd valid simulation string composition."""
+        self.assertTrue(is_valid_sim_id(1))
+        self.assertTrue(is_valid_sim_id(999999))
+        self.assertFalse(is_valid_sim_id(-1.1))
+        self.assertFalse(is_valid_sim_id(0))
+        self.assertFalse(is_valid_sim_id(1000000))
+        self.assertEqual('000001',xf_sim_id_to_str(1))
+        self.assertEqual('999999',xf_sim_id_to_str(999999))
+
+    def test_project_file(self):
+        self.grid.project_dir = TEST_PROJECT_DIR
+        self.assertEqual(TEST_PROJECT_DIR, self.grid.project_dir)
+        self.assertEqual(MULTIPOINT_SENSOR_FILE, 
+                         self.grid._multipoint_sensor_files[0])
+        
+
+    def tearDown(self):
+        pass
+    
+if __name__ == '__main__':
+    unittest.main()
--- xfmatgrid/xfmatgrid.py
+++ xfmatgrid/xfmatgrid.py
@@ -6,23 +6,91 @@ Post-process XFdtd-generated MAT-Files, regrid, and save results to disk.
 from __future__ import (absolute_import, division, generators,
                         print_function, unicode_literals)
 
-import re, os
+import os
+from glob import glob
 from scipy.io import loadmat, savemat
 import scipy.interpolate as interp
 
 class XFFieldNonUniformGrid(object):
     """A class to hold XF field data on non-uniform grid."""
     def __init__(self):
-        self._grid_units = None
+        self._project_dir = ''
+        self._multipoint_sensor_files = []
+        self._run_id = 0
+        self._sim_id = 0
+        self._field_type = ''
 
     @property
-    def units(self):
+    def project_dir(self):
         """Return the grid units."""
-        return self._grid_units
+        return self._project_dir
     
-    @units.setter
-    def units(self, units):
-        """Set the grid units."""
-        self._grid_units = units
-        
+    @project_dir.setter
+    def project_dir(self, project_dir):
+        """Set the project directory and set sensor file location."""
+        self._project_dir = project_dir
+        if os.path.exists(self._project_dir):
+            self._multipoint_sensor_files = \
+                    glob(os.path.join(self._project_dir,
+                        'Simulations', 
+                        xf_sim_id_to_str(self._sim_id), 'Run0001', 
+                        'output', '*_info.bin'))
+    @property
+    def sim_id(self):
+        """Return the current simulation id."""
+        return self._sim_id
+    
+    @sim_id.setter
+    def sim_id(self, sim_id):
+        """Set the Simulation ID."""
+        self._sim_id = sim_id
 
+    
+    
+
+# TODO: Helper functions - refactor to xf_utils/ or similar
+def is_valid_run_id(run_id):
+    """Check whether run_id is valid XFdtd run id."""
+    MIN_RUN_ID = 1
+    MAX_RUN_ID = 9999
+    if isinstance(run_id, int):
+        if (run_id >= MIN_RUN_ID) and (run_id <= MAX_RUN_ID):
+            return True
+    return False
+
+def is_valid_sim_id(sim_id):
+    """Check whether sim_id is valid XFdtd simulatin id."""
+    MIN_SIM_ID = 1
+    MAX_SIM_ID = 999999
+    if isinstance(sim_id, int):
+        if (sim_id >= MIN_SIM_ID) and (sim_id <= MAX_SIM_ID):
+            return True
+    return False
+
+def xf_run_id_to_str(run_id):
+    """Converts a integer to XFdtd RunID string."""
+    padded_string_length = 4
+    run_id_string = 'Run'
+    if is_valid_run_id(run_id):
+        run_id_string_length = len(str(run_id))
+        for i in range(padded_string_length - run_id_string_length):
+            run_id_string += '0'
+        run_id_string += str(run_id)
+        return run_id_string
+    else:
+        print("Invalid Run ID: ", run_id)
+        return None
+
+def xf_sim_id_to_str(sim_id):
+    """Converts an integer to a valid XFdtd SimID string."""
+    padded_string_length = 6
+    sim_id_string = ''
+    if is_valid_sim_id(sim_id):
+        sim_id_string_length = len(str(sim_id))
+        for i in range(padded_string_length - sim_id_string_length):
+            sim_id_string += '0'
+        sim_id_string += str(sim_id)
+        return sim_id_string
+    else:
+        print("Invalid Simulation ID: ", sim_id)
+        return None
