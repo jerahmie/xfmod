Fixed incorrect calculation of B1_Tx.

examples/plot_sum_b1_total.py
@@ -5,13 +5,14 @@ Save the B1 tx/rx field plot.
 from __future__ import(absolute_import, division, generators,
                        print_function, unicode_literals)
 
-import sys
+import sys, os
 import numpy as np
 import scipy as sp
 import scipy.io as spio
 import matplotlib.pyplot as plt
 
 def plot_main(mat_file, z_val):
+    eps = 1e-50
     """Plot the x-y cut of the data at the z_index nearest to z_val."""
     try:
         mat_data = spio.loadmat(mat_file)
@@ -22,9 +23,13 @@ def plot_main(mat_file, z_val):
         x_dim = mat_data['XDim']
         y_dim = mat_data['YDim']
         XX, YY = np.meshgrid(x_dim, y_dim, indexing='ij')
+        print('B1Tx: ',np.shape(mat_data['B1Tx']))
+        b1Mod = abs(mat_data['B1Tx'])
+        b1Mod[b1Mod < eps] = eps
         fig=plt.pcolor(XX,YY,
-                       20*np.log10(abs(mat_data['B1Tx'][:,:,z_ind])/1.e-6),
-                       cmap=plt.cm.get_cmap('bone'))
+                       20.0*np.log10(b1Mod[:,:,z_ind]/1e-6),
+                       cmap=plt.cm.get_cmap('bone'),
+                       vmin=-32, vmax=25)
  
         plt.title("|B1+|, z=" + str(z_val))
         plt.xlabel('x (m)'); plt.ylabel('y (m)')
@@ -32,8 +37,7 @@ def plot_main(mat_file, z_val):
         cb = plt.colorbar()
         cb.set_label('dB relative to microTesla/sqrt(Watt)')
         plt.tight_layout()
-        plt.savefig('test')
-
+        plt.savefig(os.path.realpath(os.path.splitext(mat_file)[0]+'.png'))
 
     except FileNotFoundError as e:
         print("The mat file was not found: ", mat_file)

examples/sum_b1_total.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Sum B1 transmit/receive fields.  B1+ = B1x +/- j*conj(B1y)
+Sum B1 transmit/receive fields.  B1+ = B1x + j*B1y.
 """
 
 from __future__ import(absolute_import, division, generators,
@@ -31,19 +31,19 @@ class B1WriterNonUniform(object):
             nu_field = xfmatgrid.XFFieldNonUniformGrid(self._xf_project_dir,
                                                        sim_id,
                                                        self._run_id)
-            field_norm = 1./np.sqrt(xfsystem.XFSystem(self._xf_project_dir, sim_id, self._run_id).net_input_power)
+            field_norm = 1./np.sqrt(xfsystem.XFSystem(self._xf_project_dir,
+                                                      sim_id,
+                                                      self._run_id).net_input_power)
+            b1_tx = 0.5 * field_norm * \
+                    (nu_field.ss_field_data('B','x') + \
+                     1j*(nu_field.ss_field_data('B','y')))
             if self._b1_tx is None:
-                self._b1_tx = field_norm * \
-                              (nu_field.ss_field_data('B','x') + \
-                              1j*np.conj(nu_field.ss_field_data('B','y')))
+                self._b1_tx = b1_tx
                 self._xdim = nu_field.xdim
                 self._ydim = nu_field.ydim
                 self._zdim = nu_field.zdim
             else:
-                self._b1_tx += field_norm * \
-                               (nu_field.ss_field_data('B','x') + \
-                                1j*np.conj(nu_field.ss_field_data('B','y')))
-
+                self._b1_tx += by_tx
 
     def export_b1_tx_mat(self, file_name):
         """