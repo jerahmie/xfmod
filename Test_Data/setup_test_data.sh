acc443b9f7439a91cc1df98e8282b8e19613f02d
Jerahmie Radder <radd0012@umn.edu>
2016-05-16 16:26:11 -0400

    Removed verbose flag and enabled quiet flag for wget. Test data is downloaded to correct directory, regardless of from where script is called.

--- Test_Data/setup_test_data.sh
+++ Test_Data/setup_test_data.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/sh
 
 # Download and extract test data for xfmod
-#https://drive.google.com/open?id=0B9waCaG162I0U2pBdkhOaEhTc3M
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GOOGLE_DRIVE_FILEID="0B9waCaG162I0U2pBdkhOaEhTc3M"
-TEST_COIL_TARGZ=Test_Coil.xf.tar.gz
+TEST_COIL_TARGZ=${DIR}/Test_Coil.xf.tar.gz
 TEST_COIL_URL="https://googledrive.com/host/${GOOGLE_DRIVE_FILEID}"
 echo "-> Downloading ${TEST_COIL_TARGZ}"
-wget --verbose $TEST_COIL_URL -O ${TEST_COIL_TARGZ}
+wget -q -o /dev/null $TEST_COIL_URL -O ${TEST_COIL_TARGZ}
 if [ -e "${TEST_COIL_TARGZ}" ]; then
     echo "-> Extracting ${TEST_COIL_TARGZ}."
-    tar xf ${TEST_COIL_TARGZ}
+    tar xf ${TEST_COIL_TARGZ} -C ${DIR}
     echo "-> Removing ${TEST_COIL_TARGZ}"
     rm ${TEST_COIL_TARGZ}
 else
