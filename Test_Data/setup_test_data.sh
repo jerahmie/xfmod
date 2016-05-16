62421a44e62d314e9e1ab58253f8673838995eb3
Jerahmie Radder <radd0012@umn.edu>
2016-05-16 13:24:31 -0400

    Updated Test_Data setup script to download and extract Test_Coil.xf project.

--- Test_Data/setup_test_data.sh
+++ Test_Data/setup_test_data.sh
@@ -1,6 +1,17 @@
-#!/usr/bin/env bash
+#!/usr/bin/sh
 
 # Download and extract test data for xfmod
-#
-echo wget Test_Coil.xf.tar.gz
-echo tar xvf Test_Coil.xf.tar.gz 
+#https://drive.google.com/open?id=0B9waCaG162I0U2pBdkhOaEhTc3M
+GOOGLE_DRIVE_FILEID="0B9waCaG162I0U2pBdkhOaEhTc3M"
+TEST_COIL_TARGZ=Test_Coil.xf.tar.gz
+TEST_COIL_URL="https://googledrive.com/host/${GOOGLE_DRIVE_FILEID}"
+echo "-> Downloading ${TEST_COIL_TARGZ}"
+wget --verbose $TEST_COIL_URL
+if [ -e "${GOOGLE_DRIVE_FILEID}" ]; then
+    echo "-> Extracting test coil data."
+    tar xvf ${GOOGLE_DRIVE_FILEID}
+    rm ${GOOGLE_DRIVE_FILEID}
+else
+    echo "[ERROR] ${TEST_COIL_TARGZ} not found."
+    exit -1
+fi
