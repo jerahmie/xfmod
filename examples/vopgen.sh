018e835277cfb7c2bb36e8221787cf193c368894
Jerahmie Radder <radd0012@umn.edu>
2016-05-17 16:09:37 -0500

    Added __init__.py to allow access to submodules.

--- examples/vopgen.sh
+++ examples/vopgen.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+XF_PROJECT=$1
 #XF_PROJECT='/mnt/DATA/XFdtd_Test_Projects/Simple_Loop.xf'
-XF_PROJECT='/Data/CMRR/xfmod/Test_Data/Test_Coil.xf'
-VOPGEN_OUT_DIR='/Data/CMRR/output'
-
-python3 ../xfwriter/vopgen/vopgen.py \
+#XF_PROJECT='/Data/CMRR/xfmod/Test_Data/Test_Coil.xf'
+###VOPGEN_OUT_DIR='/Data/CMRR/output'
+if [ -d "${XF_PROJECT}" ]; then
+    VOPGEN_OUT_DIR=${XF_PROJECT}/Export/Vopgen
+    if [ ! -d "${VOPGEN_OUT_DIR}" ]; then
+	mkdir -p ${VOPGEN_OUT_DIR}
+    fi
+    
+    python3 ../xfwriter/vopgen/vopgen.py \
         --xf_project=${XF_PROJECT} \
         --export_dir=${VOPGEN_OUT_DIR} \
         --origin='[0.0, 0.0, 0.044]' \
         --lengths='[0.256, 0.256, 0.256]' \
         --deltas='[0.002, 0.002, 0.002]'
+fi
