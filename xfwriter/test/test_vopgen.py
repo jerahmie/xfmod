ba9ba7917822ce506a52a421a387d1ba65f32ae2
Jerahmie Radder <radd0012@umn.edu>
2016-05-18 16:54:21 -0400

    fixed pylint warnings in test_vopgen.py

--- xfwriter/test/test_vopgen.py
+++ xfwriter/test/test_vopgen.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """
 Test for vopgen exporter.
 """
@@ -6,7 +5,7 @@ Test for vopgen exporter.
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
-from os import getcwd, remove
+import os
 from os.path import normpath, dirname, realpath, join, isfile
 import unittest
 from numpy import allclose, arange, shape
@@ -14,51 +13,51 @@ import scipy.io as spio
 from xfutils.xfproject import XFProjectInfo
 import xfwriter.vopgen
 
-coil_xf_path = normpath(join(realpath(__file__),
+COIL_XF_PATH = normpath(join(realpath(__file__),
                              '..', '..', '..',
                              'Test_Data',
                              'Test_Coil.xf'))
 
-xf_test_coil_powers = [5.78071000e-05, 1.27783000e-04, 1.57568000e-04]
-ef_map_array_file = normpath(join(dirname(realpath(__file__)),
+XF_TEST_COIL_POWERS = [5.78071000e-05, 1.27783000e-04, 1.57568000e-04]
+EF_MAP_ARRAY_FILE = normpath(join(dirname(realpath(__file__)),
                                   'efMapArrayN.mat'))
-property_map_file = normpath(join(dirname(realpath(__file__)),
+PROPERTY_MAP_FILE = normpath(join(dirname(realpath(__file__)),
                                   'propmap.mat'))
-sar_mask_file = normpath(join(dirname(realpath(__file__)),
+SAR_MASK_FILE = normpath(join(dirname(realpath(__file__)),
                               'sarmask_aligned.mat'))
-mass_density_map_3d_file = normpath(join(dirname(realpath(__file__)),
+MASS_DENSITY_MAP_3D_FILE = normpath(join(dirname(realpath(__file__)),
                                          'massdensityMap3D.mat'))
-sim_id = 1
-run_id = 1
+SIM_ID = 1
+RUN_ID = 1
 
 # Uniform grid resolution
-dx = 0.002 # m
-dy = 0.002 # m
-dz = 0.002 # m
+DX = 0.002 # m
+DY = 0.002 # m
+DZ = 0.002 # m
 
 # roi dimensions
-x_roi_len = 0.256 # m
-y_roi_len = 0.256 # m
-z_roi_len = 0.256 # m
+X_ROI_LEN = 0.256 # m
+Y_ROI_LEN = 0.256 # m
+Z_ROI_LEN = 0.256 # m
 
 # roi center
-x0 = 0.0
-y0 = 0.0
-z0 = 0.044
+X0 = 0.0
+Y0 = 0.0
+Z0 = 0.044
 
 # roi grid dimensions
-x_roi_dim = int(x_roi_len/dx)
-y_roi_dim = int(y_roi_len/dy)
-z_roi_dim = int(z_roi_len/dz)
+X_ROI_DIM = int(X_ROI_LEN/DX)
+Y_ROI_DIM = int(Y_ROI_LEN/DY)
+Z_ROI_DIM = int(Z_ROI_LEN/DZ)
 
 class TestVopgenWriter(unittest.TestCase):
     """Tests for vopgen matfile writers."""
     @classmethod
     def setUpClass(cls):
-        if isfile(ef_map_array_file):
-            remove(ef_map_array_file)
+        if isfile(EF_MAP_ARRAY_FILE):
+            os.remove(EF_MAP_ARRAY_FILE)
         cls.sim_ids = []
-        cls._xf_project_info = XFProjectInfo(coil_xf_path)
+        cls._xf_project_info = XFProjectInfo(COIL_XF_PATH)
         simulations = cls._xf_project_info.xf_sim_run_list
         for idx, sim in enumerate(simulations):
             if sim[0]:
@@ -73,7 +72,7 @@ class TestVopgenWriter(unittest.TestCase):
 
     def test_efn_net_input_power(self):
         """Test VopgenEFMatArrayN net input power array."""
-        tvopgen = xfwriter.vopgen.VopgenEFMapArrayN(coil_xf_path, self.sim_ids)
+        tvopgen = xfwriter.vopgen.VopgenEFMapArrayN(COIL_XF_PATH, self.sim_ids)
         self.assertEqual(3, len(tvopgen._net_input_power_per_coil))
         self.assertTrue(allclose(xf_test_coil_powers,
                                  tvopgen._net_input_power_per_coil))
@@ -83,19 +82,19 @@ class TestVopgenWriter(unittest.TestCase):
         Test the shape of data structures within the saved mat file and generate
         png images of the electric field regions for efMapArrayN.mat
         """
-        tvopgen = xfwriter.vopgen.VopgenEFMapArrayN(coil_xf_path, self.sim_ids)
-        tvopgen.set_grid_origin(x0, y0, z0)
-        self.assertEqual(x0, tvopgen._x0)
-        self.assertEqual(y0, tvopgen._y0)
-        self.assertEqual(z0, tvopgen._z0)
-        tvopgen.set_grid_len(x_roi_len, y_roi_len, z_roi_len)
-        self.assertEqual(x_roi_len, tvopgen._xlen)
-        self.assertEqual(y_roi_len, tvopgen._ylen)
-        self.assertEqual(z_roi_len, tvopgen._zlen)
+        tvopgen = xfwriter.vopgen.VopgenEFMapArrayN(COIL_XF_PATH, self.sim_ids)
+        tvopgen.set_grid_origin(X0, Y0, Z0)
+        self.assertEqual(X0, tvopgen._x0)
+        self.assertEqual(Y0, tvopgen._y0)
+        self.assertEqual(Z0, tvopgen._z0)
+        tvopgen.set_grid_len(X_ROI_LEN, Y_ROI_LEN, Z_ROI_LEN)
+        self.assertEqual(X_ROI_LEN, tvopgen._xlen)
+        self.assertEqual(Y_ROI_LEN, tvopgen._ylen)
+        self.assertEqual(Z_ROI_LEN, tvopgen._zlen)
         tvopgen.set_grid_resolution(0.002, 0.002, 0.002)
-        self.assertEqual(dx, tvopgen._dx)
-        self.assertEqual(dy, tvopgen._dy)
-        self.assertEqual(dz, tvopgen._dz)
+        self.assertEqual(DX, tvopgen._dx)
+        self.assertEqual(DY, tvopgen._dy)
+        self.assertEqual(DZ, tvopgen._dz)
         tvopgen._update_export_grid()
         self.assertTrue(allclose(arange(tvopgen._x0 - tvopgen._xlen/2.0,
                                         tvopgen._x0 + tvopgen._xlen/2.0,
@@ -106,30 +105,30 @@ class TestVopgenWriter(unittest.TestCase):
         self.assertTrue(allclose(arange(tvopgen._z0 - tvopgen._zlen/2.0,
                                         tvopgen._z0 + tvopgen._zlen/2.0,
                                         tvopgen._dz), tvopgen._zdim_uniform))
-        tvopgen.savemat(ef_map_array_file)
-        self.assertTrue(isfile(ef_map_array_file))
-        ef_map = spio.loadmat(ef_map_array_file)
-        self.assertTrue((x_roi_dim, y_roi_dim, z_roi_dim, 3, 3),
+        tvopgen.savemat(EF_MAP_ARRAY_FILE)
+        self.assertTrue(isfile(EF_MAP_ARRAY_FILE))
+        ef_map = spio.loadmat(EF_MAP_ARRAY_FILE)
+        self.assertTrue((X_ROI_DIM, Y_ROI_DIM, Z_ROI_DIM, 3, 3),
                         ef_map['efMapArrayN'])
 
     def test_propmap_mat(self):
         """
         Tests to create/read propmat.mat
         """
-        tpropmap = xfwriter.vopgen.VopgenPropertyMap(coil_xf_path,
-                                                     sim_id, run_id)
-        tpropmap.set_grid_origin(x0, y0, z0)
-        self.assertEqual(x0, tpropmap._x0)
-        self.assertEqual(y0, tpropmap._y0)
-        self.assertEqual(z0, tpropmap._z0)
-        tpropmap.set_grid_len(x_roi_len, y_roi_len, z_roi_len)
-        self.assertEqual(x_roi_len, tpropmap._xlen)
-        self.assertEqual(y_roi_len, tpropmap._ylen)
-        self.assertEqual(z_roi_len, tpropmap._zlen)
-        tpropmap.set_grid_resolution(dx, dy, dz)
-        self.assertEqual(dx, tpropmap._dx)
-        self.assertEqual(dy, tpropmap._dy)
-        self.assertEqual(dz, tpropmap._dz)
+        tpropmap = xfwriter.vopgen.VopgenPropertyMap(COIL_XF_PATH,
+                                                     SIM_ID, RUN_ID)
+        tpropmap.set_grid_origin(X0, Y0, Z0)
+        self.assertEqual(X0, tpropmap._x0)
+        self.assertEqual(Y0, tpropmap._y0)
+        self.assertEqual(Z0, tpropmap._z0)
+        tpropmap.set_grid_len(X_ROI_LEN, Y_ROI_LEN, Z_ROI_LEN)
+        self.assertEqual(X_ROI_LEN, tpropmap._xlen)
+        self.assertEqual(Y_ROI_LEN, tpropmap._ylen)
+        self.assertEqual(Z_ROI_LEN, tpropmap._zlen)
+        tpropmap.set_grid_resolution(DX, DY, DZ)
+        self.assertEqual(DX, tpropmap._dx)
+        self.assertEqual(DY, tpropmap._dy)
+        self.assertEqual(DZ, tpropmap._dz)
         tpropmap._update_export_grid()
         self.assertTrue(allclose(arange(tpropmap._x0 - tpropmap._xlen/2.0,
                                         tpropmap._x0 + tpropmap._xlen/2.0,
@@ -140,53 +139,53 @@ class TestVopgenWriter(unittest.TestCase):
         self.assertTrue(allclose(arange(tpropmap._z0 - tpropmap._zlen/2.0,
                                         tpropmap._z0 + tpropmap._zlen/2.0,
                                         tpropmap._dz), tpropmap._zdim_uniform))
-        tpropmap.savemat(property_map_file)
-        self.assertTrue(isfile(property_map_file))
-        prop_map = spio.loadmat(property_map_file)
-        self.assertEqual(x_roi_dim, len(prop_map['XDim']))
-        self.assertEqual(y_roi_dim, len(prop_map['YDim']))
-        self.assertEqual(z_roi_dim, len(prop_map['ZDim']))
-        self.assertEqual((x_roi_dim, y_roi_dim, z_roi_dim, 3),
+        tpropmap.savemat(PROPERTY_MAP_FILE)
+        self.assertTrue(isfile(PROPERTY_MAP_FILE))
+        prop_map = spio.loadmat(PROPERTY_MAP_FILE)
+        self.assertEqual(X_ROI_DIM, len(prop_map['XDim']))
+        self.assertEqual(Y_ROI_DIM, len(prop_map['YDim']))
+        self.assertEqual(Z_ROI_DIM, len(prop_map['ZDim']))
+        self.assertEqual((X_ROI_DIM, Y_ROI_DIM, Z_ROI_DIM, 3),
                          shape(prop_map['condMap']))
-        self.assertEqual((x_roi_dim, y_roi_dim, z_roi_dim, 3),
+        self.assertEqual((X_ROI_DIM, Y_ROI_DIM, Z_ROI_DIM, 3),
                          shape(prop_map['mdenMap']))
 
     def test_sarmask_aligned_mat(self):
         """
         Tests to create/read sarmask_aligned.mat.
         """
-        print("sar_mask_file: ", sar_mask_file)
-        tsarmask = xfwriter.vopgen.VopgenSarMask(coil_xf_path, sim_id, run_id)
-        tsarmask.set_grid_origin(x0, y0, z0)
-        tsarmask.set_grid_len(x_roi_len, y_roi_len, z_roi_len)
-        tsarmask.set_grid_resolution(dx, dy, dz)
-        tsarmask.savemat(sar_mask_file)
-        self.assertTrue(isfile(sar_mask_file))
-        sar_mask_mat = spio.loadmat(sar_mask_file)
-        self.assertEqual(x_roi_dim, len(sar_mask_mat['XDim']))
-        self.assertEqual(y_roi_dim, len(sar_mask_mat['YDim']))
-        self.assertEqual(z_roi_dim, len(sar_mask_mat['ZDim']))
-        self.assertEqual((x_roi_dim, y_roi_dim, z_roi_dim),
+        print("sar_mask_file: ", SAR_MASK_FILE)
+        tsarmask = xfwriter.vopgen.VopgenSarMask(COIL_XF_PATH, SIM_ID, RUN_ID)
+        tsarmask.set_grid_origin(X0, Y0, Z0)
+        tsarmask.set_grid_len(X_ROI_LEN, Y_ROI_LEN, Z_ROI_LEN)
+        tsarmask.set_grid_resolution(DX, DY, DZ)
+        tsarmask.savemat(SAR_MASK_FILE)
+        self.assertTrue(isfile(SAR_MASK_FILE))
+        sar_mask_mat = spio.loadmat(SAR_MASK_FILE)
+        self.assertEqual(X_ROI_DIM, len(sar_mask_mat['XDim']))
+        self.assertEqual(Y_ROI_DIM, len(sar_mask_mat['YDim']))
+        self.assertEqual(Z_ROI_DIM, len(sar_mask_mat['ZDim']))
+        self.assertEqual((X_ROI_DIM, Y_ROI_DIM, Z_ROI_DIM),
                          shape(sar_mask_mat['sarmask_new']))
 
     def test_massdensity_map_3d_mat(self):
         """
         Tests to create/read massdensityMat3D.mat.
         """
-        tmdenmap3d = xfwriter.vopgen.VopgenMassDensityMap3D(coil_xf_path,
-                                                        sim_id, run_id)
-        tmdenmap3d.set_grid_origin(x0, y0, z0)
-        tmdenmap3d.set_grid_len(x_roi_len, y_roi_len, z_roi_len)
-        tmdenmap3d.set_grid_resolution(dx, dy, dz)
-        tmdenmap3d.savemat(mass_density_map_3d_file)
-        self.assertTrue(isfile(mass_density_map_3d_file))
-        mdenmap3d_mat = spio.loadmat(mass_density_map_3d_file)
-        self.assertEqual(x_roi_dim, len(mdenmap3d_mat['XDim']))
-        self.assertEqual(y_roi_dim, len(mdenmap3d_mat['YDim']))
-        self.assertEqual(z_roi_dim, len(mdenmap3d_mat['ZDim']))
-        self.assertEqual((x_roi_dim, y_roi_dim, z_roi_dim),
+        tmdenmap3d = xfwriter.vopgen.VopgenMassDensityMap3D(COIL_XF_PATH,
+                                                        SIM_ID, RUN_ID)
+        tmdenmap3d.set_grid_origin(X0, Y0, Z0)
+        tmdenmap3d.set_grid_len(X_ROI_LEN, Y_ROI_LEN, Z_ROI_LEN)
+        tmdenmap3d.set_grid_resolution(DX, DY, DZ)
+        tmdenmap3d.savemat(MASS_DENSITY_MAP_3D_FILE)
+        self.assertTrue(isfile(MASS_DENSITY_MAP_3D_FILE))
+        mdenmap3d_mat = spio.loadmat(MASS_DENSITY_MAP_3D_FILE)
+        self.assertEqual(X_ROI_DIM, len(mdenmap3d_mat['XDim']))
+        self.assertEqual(Y_ROI_DIM, len(mdenmap3d_mat['YDim']))
+        self.assertEqual(Z_ROI_DIM, len(mdenmap3d_mat['ZDim']))
+        self.assertEqual((X_ROI_DIM, Y_ROI_DIM, Z_ROI_DIM),
                          shape(mdenmap3d_mat['mden3D']))
-        self.assertEqual((x_roi_dim, y_roi_dim, z_roi_dim),
+        self.assertEqual((X_ROI_DIM, Y_ROI_DIM, Z_ROI_DIM),
                          shape(mdenmap3d_mat['mden3Dm']))
 
     def tearDown(self):
