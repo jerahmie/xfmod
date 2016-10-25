5b56c0492c2ebd2c3f5a809e5007257cd9a320ec
Jerahmie Radder <radd0012@umn.edu>
2016-10-25 13:37:33 -0500

    Added windows batch file for export

---
+++ scripts/export_mesh_fields.bat
@@ -0,0 +1,48 @@
+@echo off
+set XF_PROJECT="D:\XFdtd_Test_Projects\Simple_Loop_MRT_setup.xf"
+
+set EXPORT_DIR=Export
+if  not exist %EXPORT_DIR% (
+mkdir %EXPORT_DIR%
+)
+
+set SIMID=2
+set RUNID=1
+set ORIGIN="[0.0, 0.0, 0.0]"
+set LENGTHS="[0.256, 0.256, 0.256]"
+set DELTAS="[0.002, 0.002, 0.002]"
+
+rem Material properties export and regrid
+
+python ..\xfwriter\xf_griddata_writer_uniform.py^
+ --xf_project=%XF_PROJECT% ^
+ --export_file=%EXPORT_DIR%\Simple_loop_MRT_grid.mat ^
+ --sim=%SIMID% ^
+ --run=%RUNID% ^
+ --origin=%ORIGIN% ^
+ --lengths=%LENGTHS% ^
+ --deltas=%DELTAS%
+
+rem B-field export and regrid
+
+python ..\xfwriter\xf_field_writer_uniform.py ^
+ --xf_project=%XF_PROJECT% ^
+ --export_file=%EXPORT_DIR%\Simple_loop_MRT_B.mat ^
+ --field=B ^
+ --sim=%SIMID% ^
+ --run=%RUNID% ^
+ --origin=%ORIGIN% ^
+ --lengths=%LENGTHS% ^
+ --deltas=%DELTAS%
+
+rem E-field export and regrid
+
+python ..\xfwriter\xf_field_writer_uniform.py^
+ --xf_project=%XF_PROJECT% ^
+ --export_file=%EXPORT_DIR%\Simple_loop_MRT_E.mat ^
+ --field=E ^
+ --sim=%SIMID% ^
+ --run=%RUNID% ^
+ --origin=%ORIGIN% ^
+ --lengths=%LENGTHS% ^
+ --deltas=%DELTAS%
