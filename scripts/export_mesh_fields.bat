763eff6e78cde207993deadbd7ab8ea2c01c46d3
Jerahmie Radder <radd0012@umn.edu>
2016-10-26 17:17:09 -0500

    Updated some fields in regrid windows bat file.

--- scripts/export_mesh_fields.bat
+++ scripts/export_mesh_fields.bat
@@ -1,24 +1,23 @@
 @echo off
-set XF_PROJECT="F:\Bent_dipole_comparison\Orthogonal_Duke\Bent_dipole_orthogonal_withDuke.xf"
+set XF_PROJECT="C:\Users\jerahmie\XFDTD_PROJECTS\Bent_dipole_comparison\Orthogonal_Duke\Bent_dipole_orthogonal_withDuke.xf"
 rem set XF_PROJECT="F:\Bent_dipole_comparison\45Tilted_Duke\45Tilted.xf"
-rem set XF_PROJECT="D:\XFdtd_Test_Projects\Simple_Loop_MRT_setup.xf"
+
+set SIMID=3
+set RUNID=1
+set ORIGIN="[0.002, -0.064, 0.0]"
+set LENGTHS="[0.180, 0.180, 0.180]"
+set DELTAS="[0.002, 0.002, 0.002]"
+
 
 rem Get XF project name from XF_PROJECT string
 for %%i in ("%XF_PROJECT%") do (
 set XF_PROJECT_NAME=%%~ni
 ) 
 
-set EXPORT_DIR=Export
+set EXPORT_DIR=%XF_PROJECT%\Export
 if  not exist %EXPORT_DIR% (
 mkdir %EXPORT_DIR%
 )
-
-set SIMID=3
-set RUNID=1
-set ORIGIN="[0.0, 0.0, 0.0]"
-set LENGTHS="[0.256, 0.256, 0.256]"
-set DELTAS="[0.002, 0.002, 0.002]"
-
 echo "Material properties export and regrid"
 
 python ..\xfwriter\xf_griddata_writer_uniform.py^
