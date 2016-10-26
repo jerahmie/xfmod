6af19ff08e30a6d7b7095bc81c1b65a326f099fa
Jerahmie Radder <radd0012@umn.edu>
2016-10-26 17:21:53 -0500

    Improved appearance of export_mesh_fields.bat

--- scripts/export_mesh_fields.bat
+++ scripts/export_mesh_fields.bat
@@ -1,14 +1,12 @@
 @echo off
-set XF_PROJECT="C:\Users\jerahmie\XFDTD_PROJECTS\Bent_dipole_comparison\Orthogonal_Duke\Bent_dipole_orthogonal_withDuke.xf"
-rem set XF_PROJECT="F:\Bent_dipole_comparison\45Tilted_Duke\45Tilted.xf"
-
+set XF_PROJECT="C:\xFDTD_simulation_data\Bent_dipole_comparison\Orthogonal_Duke\Bent_dipole_orthogonal_withDuke.xf"
 set SIMID=3
 set RUNID=1
 set ORIGIN="[0.002, -0.064, 0.0]"
 set LENGTHS="[0.180, 0.180, 0.180]"
 set DELTAS="[0.002, 0.002, 0.002]"
 
-
+rem --------------------------------------------------------------------------------------------------
 rem Get XF project name from XF_PROJECT string
 for %%i in ("%XF_PROJECT%") do (
 set XF_PROJECT_NAME=%%~ni
