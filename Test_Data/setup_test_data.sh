7d3a85087280b46e3127a71b3c277b203cf172d5
Jerahmie Radder <jerahmie@uwalumni.com>
2016-12-13 11:09:53 -0600

    changed setup_test_data.sh to process a list of files in a cleaner way.

--- Test_Data/setup_test_data.sh
+++ Test_Data/setup_test_data.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Webdir
+base_url="http://www2.cmrr.umn.edu/~jerahmie"
+
+# Test files
+test_files=('Custom_Cosim.xf.tar.gz' \
+                'Test_Coil.xf.tar.gz' \
+                'green_coil_7T_duke_2mm_vopgen.tar.gz' \
+                'project-s11.cti')
+
 # Check system and set variables accordingly
 os_type=`uname -s`
 if [ "${os_type}" == "Linux" ]; then
@@ -8,96 +17,85 @@ elif [ "${os_type}" == "Darwin" ]; then
     md5_bin='md5'
 fi
 
-base_url="http://www2.cmrr.umn.edu/~jerahmie"
-url_timeout=10 # seconds before timeout
+# wget params
+url_timeout=60 # seconds before timeout
 url_tries=10 # number of tries
+progress_type="bar:noscroll"
 
-# Download and extract test data for xfmod
-dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Download Test_Coil.xf project
-test_coil_targz="Test_Coil.xf.tar.gz"
-test_coil_md5sum="Test_Coil.xf.tar.gz.md5sum"
-test_coil_targz_path=${dir}/${test_coil_targz}
-test_coil_url=${base_url}/${test_coil_targz}
-test_coil_md5sum_url=${base_url}/${test_coil_md5sum}
-echo "-> Downloading ${test_coil_targz}"
-#wget --tries=${url_tries} --timeout=${url_timeout} -q -o /dev/null \
-#     ${test_coil_url}
-echo "-> Downloading ${test_coil_md5sum}"
-wget --tries=${url_tries} --timeout=${url_timeout} -q -o /dev/null \
-     ${test_coil_md5sum_url}
-
-echo ${test_coil_md5sum}
-if [ -f "${test_coil_md5sum}" ]; then
-    known_test_coil_md5sum=`sed -n 's/.*\([0-9a-f]\{32\}\).*/\1/p' ${test_coil_md5sum}`
-    rm ${test_coil_md5sum}
-else
-    echo "[ERROR] could not download ${test_coil_md5sum}"
-    exit -1
-fi
-if [ -e "${test_coil_targz}" ]; then
-    echo "-> Verify ${test_coil_targz}"
-    tc_md5sum=`${md5_bin} ${test_coil_targz} | sed -n 's/.*\([0-9a-f]\{32\}\).*/\1/p'`
-    echo ${tc_md5sum}
-    if [ "${tc_md5sum}" == "${known_test_coil_md5sum}" ]; then
-        echo "-> ${test_coil_targz} valid."
-        echo "-> Extracting ${test_coil_targz}."
-        tar xf ${test_coil_targz} -C ${dir}
-        echo "-> Removing ${test_coil_targz}"
-        rm ${test_coil_targz}
-    else
-        echo "[ERROR] ${test_coil_targz} not valid."
+# Function: md5sum_from_file
+#   Returns the md5sum from a *.md5sum file
+md5sum_from_file() 
+{
+    if [ -z "$1" ]; then
+        echo "Usage: md5sum_from_file myfile.md5sum"
         exit -1
     fi
-else
-    echo "[ERROR] ${test_coil_targz} not found."
-    exit -1
-fi
+    echo `sed -n 's/.*\([0-9a-f]\{32\}\).*/\1/p' ${1}`
+}
 
-# Download test CITI file
-citi_file="project-s11.cti"
-citi_md5sum_file="project-s11-cti.md5sum"
-citi_url=${base_url}/${citi_file}
-citi_md5sum_url=${base_url}/${citi_md5sum_file}
-citi_file_valid=0
+# Function: download_file server file
+# ex:
+#   download_file "http://my.server.com/~jerahmie" "myfile.txt"
 
-echo "-> Downloading hash file: ${citi_md5sum_file}"
-wget --tries=${url_tries} --timeout=${url_timeout} -q -o /dev/null \
-     ${citi_md5sum_url} 
-if [ -f "${citi_md5sum_file}" ]; then
-    known_citi_md5sum=`sed -n 's/.*\([0-9a-f]\{32\}\).*/\1/p' ${citi_md5sum_file}`
-else
-    echo "[ERROR] Could not download ${citi_md5sum_url}"
-    exit -1
-fi
-
-# Check if CITI file exists and is valid.  Otherwise try to download a new one.
-if [ -f "${citi_file}" ]; then
-    test_citi_md5sum=`${md5_bin} ${citi_file} | sed -n 's/.*\([0-9a-f]\{32\}\).*/\1/p'`
-    if [ "${known_citi_md5sum}" == "${test_citi_md5sum}" ]; then
-        echo "-> ${citi_file} valid."
-        citi_file_valid=1
-    else
-        echo "-> Checking for new ${citi_file}"
-        wget -q -o /dev/null ${citi_url}
-    fi
-else
-    echo "-> Downloading CITI file: ${citi_url}"
-    wget -q -o /dev/null ${citi_url}
-fi
-
-# Perform final check on CITI file, if necessary.
-if [ "${citi_file_valid}" == 0 ]; then
-    test_citi_md5sum=`${md5_bin} ${citi_file} | sed -n 's/.*\([0-9a-f]\{32\}\).*/\1/p'`
-    echo "-> Verifying ${citi_file}"
-    if [ "${known_citi_md5sum}" == "${test_citi_md5sum}" ]; then
-        echo "-> ${citi_file} valid."
-    else
-        echo "[ERROR] ${citi_file} md5sum invalid."
+download_file ()
+{
+    if [ -z "$1" ]; then
+        echo "Parameter #1 is zero length."
         exit -1
     fi
-fi
+    local download_req=1
+    local url=${1}
+    local data_file=${2}
+    local md5sum_file="${2}.md5sum"
+    local md5sum_url=${url}/${md5sum_file}
+    printf "Downloading files %s\n" ${md5sum_url}
+    local temp_dir=$(mktemp -d /tmp/test_dataXXXX)
 
-# Cleanup: remove the md5sum file
-rm ${citi_md5sum_file}
+    wget --tries=${url_tries} --timeout=${url_timeout} \
+         --progress=${progress_type} \
+         -O ${temp_dir}/${md5sum_file} ${md5sum_url}
+    if [ ! -f ${temp_dir}/${md5sum_file} ]; then
+        echo "Download error: " ${md5sum_file}
+    fi
+    local remote_md5sum=$(md5sum_from_file ${temp_dir}/${md5sum_file})
+    if [ -f "${md5sum_file}" ]; then
+        echo "Found local md5sum file: " ${md5sum_file}
+        local local_md5sum=`md5sum_from_file ${md5sum_file}`
+        if [ "${local_md5sum}" == "${remote_md5sum}" ]; then
+            echo "Checksum match: ${md5sum_file}. Skipping ${data_file} download."
+            download_req=0
+        fi
+    fi
+    
+    if [ "${download_req}" -eq 1 ]; then
+        # Download required
+        if [ -e "${data_file}" ]; then
+            rm ${data_file}
+        elif [ -e "${data_file%.tar.gz}" ]; then
+            rm -r "${data_file%.tar.gz}"
+        fi
+        wget --tries=${url_tries} --timeout=${url_timeout}  \
+             --progress=${progress_type} ${url}/${data_file}
+        local df_md5sum=`${md5_bin} ${data_file} | sed -n 's/.*\([0-9a-f]\{32\}\).*/\1/p'`
+        if [ "${df_md5sum}" != "${remote_md5sum}" ]; then
+            echo "Download checksum failed"
+            exit -1
+        else
+            if [ "${data_file: -6}" == "tar.gz"  ]; then
+                tar xf ${data_file} && rm ${data_file}
+            fi
+            mv ${temp_dir}/${md5sum_file} .
+        fi
+    fi
+    rm -r ${temp_dir}    
+}
+
+
+for idx in "${!test_files[@]}"
+do
+    download_file ${base_url} "${test_files[${idx}]}"
+done
+
+echo ""
+echo "Test data setup is complete."
+echo ""
--- xfmatgrid/xfmatgrid.py
+++ xfmatgrid/xfmatgrid.py
@@ -2,7 +2,6 @@
 Represent XFdtd field data on non-uniform grid.
 """
 
-# Ensure python 2 and 3 compatibility
 from __future__ import (absolute_import, division, generators,
                         print_function, unicode_literals)
 
