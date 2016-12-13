b71dfba8c237d753f37bf3068a4c0435e85330a3
Jerahmie Radder <jerahmie@uwalumni.com>
2016-12-13 16:36:10 -0600

    script parses metadata.xml to display project info.

--- scripts/xf_sim_summary.sh
+++ scripts/xf_sim_summary.sh
@@ -7,3 +7,26 @@ if [ ! -d "${xfproj}" ]; then
     exit -1
 fi
 
+simdirs=(${xfproj}/Simulations/*/)
+separator="-------------------------------------------------------------------"
+printf "\n%s\n" "${separator}"
+printf "Sim ID\t%s\n" "$(basename ${xfproj})"
+printf "%s\n" "${separator}"
+
+for d in ${simdirs[@]}; do
+    md="${d}/SimulationMetadata/metadata.xml"
+    if [ -f "${md}" ]; then
+        simid="$(basename ${d} | sed 's/^0*//')"
+        sname="$(sed -n 's/.*sname=\"\([^\"]*\)\".*/\1/p' ${md})"
+        printf "${simid}) ${sname}\n"
+
+        simtype="$(sed -n 's/.*SimulationType=\"\([a-zA-Z]*\).*/\1/p' ${md})"
+        printf "\tType:\t${simtype}\n" 
+
+        notes="$(sed -n 's/.*<Notes>\(.*\)<\/Notes>/\1/p' ${md} | \
+                       sed 's/%0A/\\n\\t\\t/g')"
+        printf "\tNotes:\t${notes}\n"
+
+    fi
+
+done
