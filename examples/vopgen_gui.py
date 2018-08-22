c40cd8bdd0e1e57424019ce650666649cf573971
Jerahmie Radder <radd0012@umn.edu>
2018-08-22 16:19:12 -0500

    rough vopgen export gui layout is in place

--- examples/vopgen_gui.py
+++ examples/vopgen_gui.py
@@ -2,44 +2,142 @@
 """
 TK-based GUI interface for vopgen export.
 """
+import os
+import sys
 import queue
 import threading
 from time import sleep
 import tkinter as tk
+from tkinter import filedialog
+from tkinter.scrolledtext import ScrolledText
 from tkinter import ttk
+from threading import Thread
+import multiprocessing as mp
+import multiprocessing.queues as mpq
 
+class StdoutQueue(mpq.Queue):
+    """
+    Use a multiprocessing queue to simulate stdout
+    """
+    def __init__(self, *args, **kwargs):
+        ctx = mp.get_context()
+        super(StdoutQueue, self).__init__(*args, **kwargs, ctx=ctx)
 
-class vopgen_gui(object):
+    def write(self, msg):
+        self.put(msg)
+
+    def flush(self):
+        sys.__stdout__.flush()
+
+def text_catcher(text_widget, queue):
+    """
+    Helper function to tunnel queue text to text widget. 
+    """
+    while True:
+        text_widget.insert(tk.END, queue.get())
+        text_widget.see(tk.END)
+
+class VopgenGUI(object):
     """
     Class to represent the tkinter-based vopgen exporter
     """
     def __init__(self, master):
+        self.initial_dir = os.getenv("HOME")
         self.master = master
-        self.master.title("XFmod Vopgen Export")
-        self.label = ttk.Label(master, text = "Vopgen Exporter", 
-                              font = ("Arial", 16))
-        self.label.grid(row = 0, column = 1)
 
-        self.process_button = ttk.Button(master, text = "Export",
+        # GUI element: Title
+        self.master.title("XFmod Vopgen Export")
+        ttk.Label(self.master, text = "Vopgen Exporter", 
+                  font = ("Arial", 16)).grid(row = 0, column = 0)
+        ttk.Label(self.master, text = "XF Input Project: ", 
+                  font = ("Arial", 12)).grid(row = 1, column = 0)
+        self.entry_xf_proj = ttk.Entry(self.master, text = "", exportselection = 0)
+        self.entry_xf_proj.grid(row = 1, column = 1, columnspan = 2, sticky=tk.W+tk.E)
+
+        ttk.Label(self.master, text = 'origin (mm)',
+                  font = ("Arial", 10)).grid(row = 3, column = 0)
+        ttk.Label(self.master, text = 'x0',
+                  font = ("Arial", 10)).grid(row = 2, column = 1)
+        ttk.Label(self.master, text = 'y0',
+                  font = ("Arial", 10)).grid(row = 2, column = 2)
+        ttk.Label(self.master, text = 'z0',
+                  font = ("Arial", 10)).grid(row = 2, column = 3)
+        self.entry_x0 = ttk.Entry(self.master, text = "", exportselection = 0)
+        self.entry_x0.grid(row = 3, column = 1)
+        self.entry_y0 = ttk.Entry(self.master, text = "", exportselection = 0)
+        self.entry_y0.grid(row = 3, column = 2)
+        self.entry_z0 = ttk.Entry(self.master, text = "", exportselection = 0)
+        self.entry_z0.grid(row = 3, column = 3)
+
+        ttk.Label(self.master, text = 'resolution (mm)', 
+                  font = ("Arial", 10)).grid(row = 5, column = 0)
+        ttk.Label(self.master, text = 'dx',
+                  font = ("Arial", 10)).grid(row = 4, column = 1)
+        ttk.Label(self.master, text = 'dy',
+                  font = ("Arial", 10)).grid(row = 4, column = 2)
+        ttk.Label(self.master, text = 'dz',
+                  font = ("Arial", 10)).grid(row = 4, column = 3)
+
+        self.entry_dx = ttk.Entry(self.master, text = "", exportselection = 0)
+        self.entry_dx.grid(row = 5, column = 1)
+        self.entry_dy = ttk.Entry(self.master, text = "", exportselection = 0)
+        self.entry_dy.grid(row = 5, column = 2)
+        self.entry_dz = ttk.Entry(self.master, text = "", exportselection = 0)
+        self.entry_dz.grid(row = 5, column = 3)
+
+        # GUI element: text output window
+        self.frame = tk.Frame(master)
+        self.frame.grid(row = 6, column = 0, columnspan = 7)
+        self.text_box = ScrolledText(self.frame)
+        self.text_box.pack()
+
+        # GUI element: control buttons
+        self.proj_button = ttk.Button(self.master, text = "Select",
+                                      command = self.choose_project)
+        self.proj_button.grid(row = 1, column = 3)
+        self.process_button = ttk.Button(self.master, text = "Export",
                                          command = self.export_vopgen)
-        self.process_button.grid(row = 4, column = 0)
-        self.quit_button = ttk.Button(master, text = "Quit",
+        self.process_button.grid(row = 7, column = 1)
+        self.quit_button = ttk.Button(self.master, text = "Quit",
                                       command = self.close_window)
-        
-        self.quit_button.grid(row = 4, column = 2)
-        
+        self.quit_button.grid(row = 7, column = 2)
+
+        # Instantiate and start text monitor
+        self.q = StdoutQueue()
+        monitor = Thread(target = text_catcher, args = (self.text_box, self.q))
+        monitor.daemon = True
+        monitor.start()
+
+        # Redirect stdout to tkinter textbox
+        sys.stdout = self.q
 
     def export_vopgen(self):
-        print("exporting vopgen....")
-        sleep(60)
-        print("Done.")
+        """
+        Execute vopgen exporter process.
+        """
+        print("Processing XFdtd Project: ", self.entry_xf_proj.get())
 
+    def choose_project(self):
+        """
+        Choose the XF project directory.
+        """
+        directory_path = filedialog.askdirectory(mustexist = True,
+                                                 initialdir = self.initial_dir,
+                                                 title = "XFdtd Project Path")
+        print("Found project path: ", directory_path)
+        par_dir, proj_dir = os.path.split(directory_path)
+        self.initial_dir = par_dir
+        self.entry_xf_proj.delete(0, 'end')
+        self.entry_xf_proj.insert(0, directory_path)
+
+        return directory_path
+    
     def close_window(self):
+        """
+        close main window and exit.
+        """
         self.master.quit()
-        
-def main():
-    vg = vopgen_gui(tk.Tk())
-    tk.mainloop()
 
 if __name__ == '__main__':
-    main()
+    vg = VopgenGUI(tk.Tk())
+    tk.mainloop()
--- scripts/vopgen.sh
+++ scripts/vopgen.sh
@@ -6,7 +6,8 @@ echo $PYTHONPATH
 #source $HOME/venv35/bin/activate
 #XF_PROJECT='/mnt/Data/XFdtd_Projects/Stripline_10p5T_MRT_no_feeds.xf'
 #XF_PROJECT='/mnt/Data/XFdtd_Projects/10p5T_8CH_EndLoadedDipole_Test_Sim_Tissue.xf'
-XF_PROJECT='/mnt/Data/XFdtd_Projects/10p5T_8CH_EndLoadedDipole_Simple_Phantom_MK.xf'
+#XF_PROJECT='/mnt/Data/XFdtd_Projects/10p5T_8CH_EndLoadedDipole_Simple_Phantom_MK.xf'
+XF_PROJECT='/mnt/d/XFdtd_Projects/Stripline_10p5T_MRT_16Jul2018.xf'
 if [ ! -d "${XF_PROJECT}" ]; then
     echo "Could not find ${XF_PROJECT}"
     exit
@@ -19,8 +20,8 @@ else
     python ../xfmod/xfwriter/vopgen/vopgen.py \
         --xf_project=${XF_PROJECT} \
         --export_dir=${VOPGEN_OUT_DIR} \
-        --origin='[0.0, 0.0, 0.025]' \
-        --lengths='[0.335, 0.335, 0.465]' \
+        --origin='[0.0, 0.0, 0.175]' \
+        --lengths='[0.3, 0.3, 0.35]' \
         --deltas='[0.002, 0.002, 0.002]'
 fi
 
