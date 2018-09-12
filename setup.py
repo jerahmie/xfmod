1a92a33a79fdd73cca23cbf176a3ae96ae206d21
radd0012 <radd0012@umn.edu>
2018-09-12 11:58:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Uses distutils to install python module.
 from setuptools import setup
 
 setup(name='xfmod',
-      version='1.0.1',
+      version='1.1',
       py_modules=['xfmod']
       #, 'xfmatgrid', 'xfsystem', 'xfutils', 'xfwriter']
 )
